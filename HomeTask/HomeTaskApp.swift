e04e0daf56b778f96bd6f551832bcc5e237ff6f7
spycwolf <spycwolfxxx@gmail.com>
2022-02-12 14:47:45 +0900

    Initial Commit

---
+++ HomeTask/HomeTaskApp.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTaskApp.swift
+//  HomeTask
+//
+//  Created by 金子宏太 on 2022/02/12.
+//
+
+import SwiftUI
+
+@main
+struct HomeTaskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
